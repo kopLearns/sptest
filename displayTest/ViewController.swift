f3a44f3460d57d02d6673b69a96b32ecc873387e
mustafacode <spironune@Spiros-MacBook-Air.local>
2016-02-24 18:21:25 -0500

    Test

---
+++ displayTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  displayTest
+//
+//  Created by Spiro Nune on 2/24/16.
+//  Copyright © 2016 Spiro Nune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var test: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
