451703cbb4f98a84aae36fb54d45779ba22bb6f0
mustafacode <spironune@spiros-macbook-air.local>
2016-02-24 18:27:55 -0500

    Progress

--- displayTest/ViewController.swift
+++ displayTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var test: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
